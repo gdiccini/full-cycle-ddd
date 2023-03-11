e6d3b4818e55d3bfa8dbbf79056dad25cd8e3f57
Giovani Diccini <giovani.diccini@gmail.com>
2023-03-11 09:13:38 -0300

    ✨: implements address like a value object example

---
+++ aluno/src/entity/address.ts
@@ -0,0 +1,31 @@
+// Exemplo de um objeto de valor. Uma classe que apenas carrega consigo propriedades que são imutáveis, ele se auto-valida e não possui id
+
+export default class Address {
+	street: string
+	number: number
+	city: string
+	zipCode: string
+
+	constructor(street: string, number: number, city: string, zipCode: string) {
+		this.street = street
+		this.number = number
+		this.city = city
+		this.zipCode = zipCode
+
+		this.validate()
+	}
+
+	public validate() {
+		if (
+			!this.street.length ||
+			!this.number ||
+			!this.city.length ||
+			this.zipCode.length
+		)
+			throw new Error('Dados de endereço inválidos.')
+	}
+
+	public toString() {
+		return `${this.street} ${this.number}, ${this.city}`
+	}
+}
---
+++ aluno/src/entity/custumer.ts
@@ -0,0 +1,38 @@
+import Address from './address'
+
+class Customer {
+	/* Uma entidade é unica pois ela possui um id */
+	_id: string
+
+	/* As outras propriedades da entidade são mutáveis */
+	_name: string
+
+	/* Endereço possui uma expressividade muito maior agora, visto que ele tem uma tipagem muito mais caracterizada para o que ele representa */
+	_address: Address
+
+	constructor(id: string, name: string, address: Address) {
+		this._id = id
+		this._name = name
+		this._address = address
+	}
+
+	get id(): string {
+		return this.id
+	}
+
+	get name(): string {
+		return this._name
+	}
+
+	get address(): Address {
+		return this._address
+	}
+
+	set name(name: string) {
+		this._name = name
+	}
+
+	set address(address: Address) {
+		this._address = address
+	}
+}
